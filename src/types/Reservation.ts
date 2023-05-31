25ed0d24b34979ff13bb48c1474635c6a01b7090
Julian Buecher <julian.buecher@gmx.de>
2023-05-31 10:16:16 +0200

    fix * Dockerfiles and Config adjusted for pnpm

--- src/types/Reservation.ts
+++ src/types/Reservation.ts
@@ -1,6 +1,5 @@
 import { AuthorizationActions } from './Authorization';
 import CreatedUpdatedProps from './CreatedUpdatedProps';
-import User from './User';
 
 export default interface Reservation extends CreatedUpdatedProps {
   id: number;
